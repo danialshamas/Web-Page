6f13777bb7ca5dba27c4e5aeb6f1daca1eeb2c6d
danialshamas <daniyalshamas6@gmail.com>
2020-11-27 17:26:08 +0500

    Changes done

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
-console.log('here\'s a hidden message');
-
 
 let today = new Date();
 let formatDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
+
+console.log('here\'s a hidden message');
